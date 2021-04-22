8c7418ec8391b838fd302b1b2afc909e04c28f4f
Ross Correia <rmcorre@gmail.com>
2021-04-22 23:07:59 +0000

    Delete entities that were used to group concepts, techs, and tools. Moved concept and tech entities to industry package.

--- src/main/java/org/academiadecodigo/codezillas/resumeRest/repository/profile/ToolGroupJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.academiadecodigo.codezillas.resumeRest.repository.profile;
-
-import org.academiadecodigo.codezillas.resumeRest.domainModel.profile.tool.ToolGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToolGroupJpaRepository extends JpaRepository<ToolGroup, Integer> {
-}
