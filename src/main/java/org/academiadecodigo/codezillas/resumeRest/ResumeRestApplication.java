7d6f24cf624c39b33f4c7920665e54bbf0377369
Ross Correia <rmcorre@gmail.com>
2021-08-03 17:18:50 +0000

    Configure both http and https support.

--- src/main/java/org/academiadecodigo/codezillas/resumeRest/ResumeRestApplication.java
+++ src/main/java/org/academiadecodigo/codezillas/resumeRest/ResumeRestApplication.java
@@ -1,13 +1,21 @@
 package org.academiadecodigo.codezillas.resumeRest;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ResumeRestApplication {
+
+	//HTTP port
+	@Value("${http.port}")
+	private int httpPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResumeRestApplication.class, args);
@@ -18,9 +26,22 @@ public class ResumeRestApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/*").allowedOrigins("*").allowedHeaders("*");
 			}
 		};
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer()  {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
 	}
 
 }
